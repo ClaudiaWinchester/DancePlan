873a40c364164afdda338dc3e25a3e5dcde7ad8e
ClaudiaWinchester <hiclaudia17@hotmail.com>
2019-02-09 23:00:00 -0500

    Add files via upload
    Demo realizado como ensayo de Xamarin en Visual Studio.
    
    Canal de Jesse Dietrichson: https://www.youtube.com/watch?v=LlfqXrkh1-Y&list=PLhq7kqloVlM_sVLVccVMNk1BvLhFS8Zn2

---
+++ Demo1/Demo1/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace Demo1
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+            UILabel label = new UILabel();
+            label.Text = "Gomitas";
+            label.Frame = new CoreGraphics.CGRect(50, 50, 250, 50);
+
+            View.AddSubview(label);
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
